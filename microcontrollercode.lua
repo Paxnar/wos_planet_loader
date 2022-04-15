cac34e15fd2bfbb8f304e0ead254f1c6f492c280
PavelUkilevich <ukilevich@yandex.eu>
2022-04-15 23:50:04 +0300

    shows planets

--- microcontrollercode.lua
+++ microcontrollercode.lua
@@ -4,14 +4,16 @@ local Screen = GetPartFromPort(1, "TouchScreen")
 local keyboard = GetPartFromPort(2, "Keyboard")
 local systemcoords = {}
 local jsons = {}
+local kb = true
+Screen:ClearElements()
 
 function mysplit (inputstr, sep)
         if sep == nil then
                 sep = "%s"
         end
         local t={}
-        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-                table.insert(t, str)
+        for w in (inputstr .. sep):gmatch("([^"..sep.."]*)"..sep) do
+          table.insert(t, w)
         end
         return t
 end
@@ -35,29 +37,65 @@ local confirm = Screen:CreateElement("TextButton", {
 	Position = UDim2.new(0, 0, 0.6666, 0);
 })
 
-local kbconnection = Keyboard:Connect("TextInputted",function(msg, plr)
-coordslabel:ChangeProperties({Text = msg.gsub(msg,"\n","")})
+local kbconnection = keyboard:Connect("TextInputted",function(msg, plr)
+if kb then
+    coordslabel:ChangeProperties({Text = msg.gsub(msg,"\n","")})
+end
 end)
 
 local buttonconfirm = confirm.MouseButton1Up:Connect(function(x, y)
-    if coordslabel.Text != '' then
-      systemcoords = mysplit(coordslabel, ', ')
+    if coordslabel.Text ~= '' and kb then
+      systemcoords = mysplit(coordslabel.Text, ', ')
+      print(systemcoords[1], systemcoords[2])
+      kb = false
+      Screen:ClearElements()
+      for i, v in map:GetBodies() do
+       local bodycoords = mysplit(i, ', ')
+       telescope:Configure( { ViewCoordinates = tostring(systemcoords[1])..', '..tostring(systemcoords[2])..', '..bodycoords[1]..', '..bodycoords[2]..', '..'false'})
+       wait(2)
+       local coordsinfo = telescope:GetCoordinate(tonumber(systemcoords[1]), tonumber(systemcoords[2]), tonumber(bodycoords[1]), tonumber(bodycoords[2]))
+
+       local coord = mysplit(mysplit(coordsinfo['Name'], ' ')[3], '-')
+coord[table.maxn(coord)] = string.gsub(coord[table.maxn(coord)],"%)","")
+if coord[1] == '(' then
+	coord[2] = '-'..coord[2]
+  table.remove(coord, 1)
+end
+if string.find(coord[1], '%(') then
+  coord[1] = string.gsub(coord[1],"%(","")
+end
+if coord[2] == '' then
+	coord[table.maxn(coord)] = '-'..coord[table.maxn(coord)]
+  table.remove(coord, 2)
+end
+if string.sub(coord[1], 1, 1) == '-' then
+	coord[1] = 0 - tonumber(string.sub(coord[1], 2, 2), 16)
+else
+  coord[1] = tonumber(coord[1], 16)
+end
+if string.sub(coord[2], 1, 1) == '-' then
+	coord[2] = 0 - tonumber(string.sub(coord[2], 2, 2), 16)
+else
+  coord[2] = tonumber(coord[2], 16)
+end
+
+for i, v in pairs(coord) do
+  print(i, v)
+end
+
+       local planet = Screen:CreateElement("ImageButton", {
+	    Image = "rbxassetid://483228156";
+	    BackgroundTransparency = 1.0;
+	    Size = UDim2.new(0.08, 0, 0.08, 0);
+	    Position = UDim2.new(30 * (12 + coord[1]) / 750, 0, 30 * (12 - coord[2]) / 750, 0);
+	    ImageColor3 = coordsinfo['Color']
+       })
+       --table.insert(jsons, coordsinfo)
+      end
     end
 end)
-print(systemcoords)
-kbconnection:Disconnect()
-buttonconfirm:Disconnect()
-typecoords:Destroy()
-coordslabel:Destroy()
-confirm:Destroy()
 
-for i, v in map:GetBodies() do
-    local bodycoords = mysplit(i, ', ')
-    telescope:Configure( { ViewCoordinates = tostring(systemcoords[1])..', '..tostring(systemcoords[2])..', '..bodycoords[1]..', '..bodycoords[2]..', '..'false')} )
-    wait(2)
-    local coordsinfo = telescope:GetCoordinate(tonumber(systemcoords[1]), tonumber(systemcoords[2]), tonumber(bodycoords[1]), tonumber(bodycoords[2]))
-    table.insert(jsons, coordsinfo)
-    end
+
 
 for i, v in pairs(jsons):
     local planet = Screen:CreateElement("ImageButton", {
